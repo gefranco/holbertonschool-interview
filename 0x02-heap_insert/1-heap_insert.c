6102cb6f601e88a18a8702236c2f8b71ebfa1e24
gefranco <933@holbertonschool.com>
2020-12-23 15:20:29 +0000

    [add] betty and documentation

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * heap_insert - inserts a value into a Max Binary Heap
+ * @root: Pointer to the root node
+ * @value: Integer stored in the node
+ * Return: created node
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	static int count;
 	heap_t *node, *current;
 	int *n_path;
 	int i;
+
 	count++;
 	node = malloc(sizeof(heap_t));
 	if (count == 1)
@@ -17,93 +23,98 @@ heap_t *heap_insert(heap_t **root, int value)
 		(*root)->n = value;
 		(*root)->left = NULL;
 		(*root)->right = NULL;
-		return *root;
+		return (*root);
 	}
-	/*if(count == 2)
-	{
-		node->parent = *root;
-		node->n = value;
-		(*root)->left = node;
-		node->left = NULL;
-		node->right = NULL;
-		return node;
-	}
-	
-	if(count == 3)
-	{
-		node->parent = *root;
-                node->n = value;
-                (*root)->right = node;
-                node->left = NULL;
-                node->right = NULL;
-		return node;
-	}*/	
 	n_path = path(count);
-	i=0;
+	i = 0;
 	current = *root;
-	while(n_path[i]!=-1)
-	{
-		if(current->left == NULL || current->right == NULL)
-			break;
-		if(n_path[i]==0)
-			current=current->left;
-		else if(n_path[i]==1)
-			current=current->right;
-			
-		i++;
-	}
-	
-	if(n_path[i] == 0)
+
+	i = get_current_node(&current, n_path);
+	if (n_path[i] == 0)
 	{
 		node->parent = current;
-                node->n = value;
-                current->left = node;
-                node->left = NULL;
-                node->right = NULL;
-                return node;
+		node->n = value;
+		current->left = node;
+		node->left = NULL;
+		node->right = NULL;
+		return (node);
 
-	}	
+	}
 	else
 	{
 		node->parent = current;
-                node->n = value;
-                current->right = node;
-                node->left = NULL;
-                node->right = NULL;
-                return node;
+		node->n = value;
+		current->right = node;
+		node->left = NULL;
+		node->right = NULL;
+		return (node);
 	}
-	return node;
+	return (node);
 }
+/**
+ * get_current_node - traverse the path to the node to insert a newone
+ * @current: Pointer to the root node
+ * @n_path: path to traverse 0-left 1-right
+ * Return: the position of the node
+ */
+int get_current_node(heap_t **current, int *n_path)
+{
+	int i = 0;
+
+	while (n_path[i] != -1)
+	{
+		if ((*current)->left == NULL || (*current)->right == NULL)
+			return (i);
+		if (n_path[i] == 0)
+			*current = (*current)->left;
+		else if (n_path[i] == 1)
+			*current = (*current)->right;
+
+		i++;
+	}
+	return (i);
+}
+/**
+ * print_path - helper function
+ * @path: --
+ * Return nothing
+ */
 void print_path(int *path)
 {
 	int i = 0;
-	while(path[i]!= -1)
+
+	while (path[i] != -1)
 	{
 		printf("%d  ", path[i]);
 		i++;
 	}
 }
+/**
+ * path - The path to go to the node to insert a newone
+ * @count: the position of the node
+ * Return: array of 1 and 0 representing the path
+ */
+
 int *path(int count)
-{	
+{
 	int j;
 	int i = 0;
 	int *path;
-	
-	int bkcount = count*2;
+
+	int bkcount = count * 2;
+
 	count = count * 2;
-	while((count=count/2) > 1)
+	while ((count = count / 2) > 1)
 	{
-		
-		
 		i++;
 	}
-	
-	path = malloc(sizeof(int)*i + 1);
-	
-	path[i] = -1;	
-	for(j = i - 1;(bkcount=bkcount/2) > 1 ;j--)
+
+	path = malloc(sizeof(int) * i + 1);
+
+	path[i] = -1;
+	for (j = i - 1; (bkcount = bkcount / 2) > 1 ; j--)
 	{
-		path[j] = bkcount%2;
-	}	
-	return path;
+		path[j] = bkcount % 2;
+	}
+	return (path);
 }
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -20,5 +20,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 heap_t *heap_insert(heap_t **root, int value);
 int *path(int count);
-
+int get_current_node(heap_t **current, int *n_path);
 #endif
