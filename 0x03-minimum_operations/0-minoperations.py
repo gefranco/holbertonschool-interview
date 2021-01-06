9895156786e393e5992f71910458f8288448845f
gefranco <933@holbertonschool.com>
2021-01-06 14:45:16 +0000

    [add] doc

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+script with the function minOperations(n)
+"""
+
+
 def minOperations(n):
+    """
+    calc the fewest number of op needed to result in exactly n H characters
+    """
     numbers = []
     bn = 2
     while bn <= n:
