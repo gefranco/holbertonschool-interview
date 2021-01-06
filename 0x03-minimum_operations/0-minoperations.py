928bf99b469f16c6bbccf32e65247f0c7118cc72
gefranco <933@holbertonschool.com>
2021-01-06 14:36:32 +0000

    [fix] pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def minOperations(n):
-	numbers = []
-	#numbers.append(n)
-	bn = 2;
-	while bn <= n:
-		if n % bn == 0:
-			n = n / bn
-			numbers.append(bn)
-		else:
-			bn += 1
-	print(numbers)
-	return sum(numbers)
+    numbers = []
+    bn = 2
+    while bn <= n:
+        if n % bn == 0:
+            n = n / bn
+            numbers.append(bn)
+        else:
+            bn += 1
+    return sum(numbers)
