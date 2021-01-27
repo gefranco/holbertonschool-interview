3659605c765ad93fa9243fd8818fe149658a4143
gefranco <933@holbertonschool.com>
2021-01-27 15:07:08 +0000

    [fix] main

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,34 +4,34 @@ script that read from standard input and compute metrics
 """
 
 import sys
-
-lines_read = 0
-total_size = 0
-stats = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+if __name__ == "__main__":
+    lines_read = 0
+    total_size = 0
+    stats = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
 # for line in sys.stdin:
-while True:
-    try:
-        line = input()
-        columns = line.split()
-        code_size = columns[-2:]
-        lines_read += 1
-        total_size = total_size + int(code_size[1])
-        stats[code_size[0]] = stats[code_size[0]] + 1
-        if lines_read == 10:
+    while True:
+        try:
+            line = input()
+            columns = line.split()
+            code_size = columns[-2:]
+            lines_read += 1
+            total_size = total_size + int(code_size[1])
+            stats[code_size[0]] = stats[code_size[0]] + 1
+            if lines_read == 10:
+                print("File size: {:d}".format(total_size))
+                for k, v in sorted(stats.items()):
+                    print(k, v)
+                lines_read = 0
+        except Exception:
             print("File size: {:d}".format(total_size))
-            for k, v in sorted(stats.items()):
+            for k, v in stats.items():
                 print(k, v)
-            lines_read = 0
-    except Exception:
-        print("File size: {:d}".format(total_size))
-        for k, v in stats.items():
-            print(k, v)
