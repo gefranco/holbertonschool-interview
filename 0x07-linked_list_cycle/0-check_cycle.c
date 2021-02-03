57c6edb43789a13f3e1a4fe656e802d529ec82de
gefranco <933@holbertonschool.com>
2021-02-03 23:03:42 +0000

    [testing]

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -5,7 +5,9 @@ int check_cycle(listint_t *list)
 	listint_t *current, *faster;
 
 	current = list;
-	faster = list->next->next;
+	if (list->next)
+		faster = list->next->next;
+
 	while (current != NULL && faster != NULL)
 	{
 		if (current == faster)
