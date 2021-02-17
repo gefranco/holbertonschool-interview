3f0cdfc4172dd4618650e95cd88982d382a73cfd
gefranco <933@holbertonschool.com>
2021-02-17 23:39:02 +0000

    [testing]

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,11 +12,12 @@ def validUTF8(data):
     for byte_data in data:
         # if bin(byte_data).split('b')[1].zfill(8)[0] is "0":
         #    continue
-        if next_bytes > 0:
+        if next_bytes > 1:
             # print("!!")
             if bin(byte_data).split('b')[1].zfill(8)[:2] is not "10":
                 return False
             next_bytes -= 1
+            continue
         if bin(byte_data).split('b')[1].zfill(8)[0] is "0":
             continue
         byte = bin(byte_data).split('b')[1].zfill(8)
