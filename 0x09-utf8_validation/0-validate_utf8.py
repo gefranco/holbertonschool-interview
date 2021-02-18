3cb0810d4e177009a661b5aa8c97153b3416c85c
gefranco <933@holbertonschool.com>
2021-02-18 14:55:10 +0000

    [fix]rftrn

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,23 +6,24 @@ validateUTF8 method file container
 
 def validUTF8(data):
     """
-    Method that validates if a set of data is UTF8 encoding
+    Method that validates if a set of data is UTF8 encoded
     """
     next_bytes = 0
     for byte_data in data:
         # if bin(byte_data).split('b')[1].zfill(8)[0] is "0":
         #    continue
+        byte = bin(byte_data).split('b')[1].zfill(8)[-8:]
+        # print(byte)
         if next_bytes > 0:
             # print("!!")
-            if bin(byte_data).split('b')[1].zfill(8)[-8:][:2] is not "10":
+            if byte[:2] is not "10":
                 return False
             next_bytes -= 1
             if next_bytes is 1:
                 next_bytes = 0
             continue
-        if bin(byte_data).split('b')[1].zfill(8)[-8:][0] is "0":
+        if byte[0] is "0":
             continue
-        byte = bin(byte_data).split('b')[1].zfill(8)[-8:]
         # print(bin(byte_data).split('b')[1].zfill(8))
         # print(byte)
         for bit in byte:
