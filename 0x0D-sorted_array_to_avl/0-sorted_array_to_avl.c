b37d53aa5d843337b30fc973cba47d0a1e98e770
gefranco <933@holbertonschool.com>
2021-03-24 17:06:54 +0000

    [fix]

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -49,5 +49,7 @@ avl_t *array_to_avl(int *array, size_t start, size_t end)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
+	if (size == 0)
+		return (NULL);
 	return (array_to_avl(array, 0, size - 1));
 }
