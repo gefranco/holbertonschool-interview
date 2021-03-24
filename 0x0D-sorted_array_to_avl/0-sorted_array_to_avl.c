9882f37ffe91a995f45f9eec4d59ab456292ea4f
gefranco <933@holbertonschool.com>
2021-03-24 16:07:32 +0000

    [fix] betty

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -6,6 +6,7 @@ avl_t *array_to_avl(int *array, size_t start, size_t end)
 {
 	avl_t *node;
 	size_t midle =  (start + end) / 2;
+
 	node = malloc(sizeof(avl_t));
 	if (node == NULL)
 	{
@@ -15,21 +16,21 @@ avl_t *array_to_avl(int *array, size_t start, size_t end)
 	else if (start >= end)
 	{
 		node->n = array[midle];
-		
-		return node;
-	}	
-	node->n = array[midle];	
+
+		return (node);
+	}
+	node->n = array[midle];
 	node->left = array_to_avl(array, start, midle - 1);
-	if(node->left != NULL)
+	if (node->left != NULL)
 		node->left->parent = node;
 
-	node->right = array_to_avl(array, midle + 1, end);	
+	node->right = array_to_avl(array, midle + 1, end);
 	node->right->parent = node;
 	node->right->parent = node;
 
-	return node;
+	return (node);
 }
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	return array_to_avl(array, 0, size - 1);
+	return (array_to_avl(array, 0, size - 1));
 }
