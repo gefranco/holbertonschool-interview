d5bbe66f2cb27b511d7cea9e24da04e4a9d5b388
gefranco <933@holbertonschool.com>
2021-03-31 23:25:15 +0000

    [testing]

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -2,43 +2,49 @@
 #include <math.h>
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-
-	skiplist_t *last;
-
+	skiplist_t *prev;
+	size_t last_index;
+	/*while(list)
+	{
+				
+		printf("%ld\n", list->index);
+		list = list->express;
+	}
+	*/
 	while (list->express)
 	{
-		last = list;
+		prev = list;
 		list = list->express;
-
 		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
-		if (value < list->n)
+		if (list->n > value)
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n",
-				last->index, list->index);
-			while (last->index != list->index)
+				prev->index, list->index);
+			while (prev->index <= list->index)
 			{
-				printf("Value checked at index [%ld] = [%d]\n", last->index, last->n);
-				if (value == last->n)
-					return (last);
-				last = last->next;
+				printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
+				if (value == prev->n)
+					return (prev);
+				prev = prev->next;
 			}
 		}
-		else if (!list->express)
-		{
-			printf("Value found between indexes [%ld] and [%ld]\n",
-                                list->index, list->index - 1 + (list->index - last->index));
-                        while (list)
-                        {
-                                printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
-                                if (value == list->n)
-                                        return (list);
-				if (!list->next)	
-					return(NULL);
-				list = list->next;
-                        }
-		}
+	}
+	prev = list;
+	last_index = list->index - 1;
+	while (prev)
+	{
+		prev = prev->next;
+		last_index++;
 	}
 
-
+	printf("Value found between indexes [%ld] and [%ld]\n",
+                                list->index, last_index);
+	while(list)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", list->index, list->n);
+                if (value == list->n)
+                	return (list);
+		list = list->next;
+	}
 	return (NULL);
 }
