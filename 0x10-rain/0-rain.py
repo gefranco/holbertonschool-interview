9b608b1eaab09921c73e77e54bf4017d282eb13d
gefranco <933@holbertonschool.com>
2021-04-16 15:03:58 +0000

    [fix]

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -21,13 +21,14 @@ def rain(walls):
         elif walls[indice] > 0 and wall_2 == 0:
             wall_2 = walls[indice]
             indice_2 = indice
-            while indice < len(walls) and walls[indice] >= wall_2:
-                wall_2 = walls[indice]
-                indice += 1
-                indice_2 = indice
+            if greater_wall(wall_2, walls[indice_2:-1]):
+                while indice < len(walls) and wall_2 < wall_1:
+                    wall_2 = walls[indice]
+                    indice += 1
+                    indice_2 = indice
         if wall_1 > 0 and wall_2 > 0:
             collect_water += collect(
-                    walls[indice_1:indice_2 - 1], wall_1, wall_2)
+                    walls[indice_1:indice_2], wall_1, wall_2)
             wall_1 = wall_2
             indice_1 = indice_2
             wall_2 = 0
@@ -42,5 +43,16 @@ def collect(water, wall_1, wall_2):
     amount = wall_2 if wall_1 > wall_2 else wall_1
     total_water = 0
     for part_water in water:
-        total_water += amount - part_water
+        if amount - part_water > 0:
+            total_water += amount - part_water
     return total_water
+
+
+def greater_wall(greater_wall, walls):
+    """
+    Function that check if there is a greater wall
+    """
+    for wall in walls:
+        if wall > greater_wall:
+            return True
+    return False
