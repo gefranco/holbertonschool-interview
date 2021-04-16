7353911543a4db9c785686e1caab677ed22d1d47
gefranco <933@holbertonschool.com>
2021-04-16 01:02:17 +0000

    [fix]

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 def rain(walls):
-	rain = 0
-	wall_1 = 0
-	rain_collected = 0
-	rain_count = 0;
-	
-	first_wall = 0;
+	wall_1 = 0;
+	wall_2 = 0;
+
+	collect_water = 0;
+	indice_1 = 0
+	indice_2 = 0
+	indice = 0;
 	for wall in walls:
-		if wall > wall_1:
-			first_wall = 1
+		if wall > 0 and wall_1 == 0:
 			wall_1 = wall
-			if first_wall == 1:
-				rain_count = wall_1
-			continue
-		if wall <  wall_1:
-			rain_collected += rain_count - wall
-			
-	return rain_collected;
+			indice_1 = indice
+		elif wall > 0 and wall_2 == 0:
+			wall_2 = wall;
+			indice_2 = indice
+		if wall_1 > 0 and wall_2 > 0:
+			collect_water +=collect(walls[indice_1 + 1 : indice_2], wall_1, wall_2)
+			wall_1 = wall_2
+			indice_1 = indice_2
+			wall_2 = 0
+		indice += 1
+	return collect_water
+
+def collect(water, wall_1, wall_2):
+	amount = wall_2 if wall_1 > wall_2 else wall_1
+	total_water = 0
+	for part_water in water:
+		total_water += amount - part_water
+	return total_water
