09cb21665e2d1c4a6fbf181cd23dfd330bc2d327
gefranco <933@holbertonschool.com>
2021-04-16 14:15:13 +0000

    [testing]

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,24 +10,24 @@ def rain(walls):
     """
     wall_1 = 0
     wall_2 = 0
-
     collect_water = 0
     indice_1 = 0
     indice_2 = 0
     indice = 0
-    for wall in walls:
-        if wall > 0 and wall_1 == 0:
-            wall_1 = wall
+    while indice < len(walls):
+        if walls[indice] > 0 and wall_1 == 0:
+            wall_1 = walls[indice]
             indice_1 = indice
-        elif wall > 0 and wall_2 == 0:
-            wall_2 = wall
+        elif walls[indice] > 0 and wall_2 == 0:
+            wall_2 = walls[indice]
             indice_2 = indice
-            if indice + 1 < len(walls) and walls[indice + 1] > wall_2:
-                wall_2 = walls[indice + 1]
-                indice_2 = indice + 1
+            while indice < len(walls) and walls[indice] >= wall_2:
+                wall_2 = walls[indice]
+                indice += 1
+                indice_2 = indice
         if wall_1 > 0 and wall_2 > 0:
             collect_water += collect(
-                    walls[indice_1 + 1:indice_2], wall_1, wall_2)
+                    walls[indice_1:indice_2 - 1], wall_1, wall_2)
             wall_1 = wall_2
             indice_1 = indice_2
             wall_2 = 0
@@ -39,7 +39,6 @@ def collect(water, wall_1, wall_2):
     """
     Function that collect the water between two walls
     """
-
     amount = wall_2 if wall_1 > wall_2 else wall_1
     total_water = 0
     for part_water in water:
