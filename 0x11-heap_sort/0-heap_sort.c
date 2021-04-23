b524b8a746a03c027098be89bca19ae4a2e6f035
gefranco <933@holbertonschool.com>
2021-04-23 01:28:47 +0000

    initial commit

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "sort.h"
+
+
+void heap_sort(int *array, size_t size)
+{
+	(void) array;
+	(void) size;
+
+	if (!array)
+		return;	
+	
+}
+
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,6 @@
+#ifndef _SORT_
+#define _SORT_
+#include <stdlib.h>
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+#endif
