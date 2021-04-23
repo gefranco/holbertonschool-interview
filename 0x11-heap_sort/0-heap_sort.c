130319746507c67a4f526ff35898e58240f9863c
gefranco <933@holbertonschool.com>
2021-04-23 01:33:28 +0000

    ...

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "sort.h"
 
-
+/**
+ * heap_sort - sorts an array in ascending order using the Heap sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * Return: nothing
+ */
 void heap_sort(int *array, size_t size)
 {
 	(void) array;
 	(void) size;
 
 	if (!array)
-		return;	
-	
+		return;
 }
 
