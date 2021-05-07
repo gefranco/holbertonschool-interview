f0872dfc3074dba55aa946c10c3e5a375900bcfa
gefranco <933@holbertonschool.com>
2021-05-07 20:34:42 +0000

    [fix]

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -15,10 +15,9 @@ def count_words(subreddit, word_list, after=None, dic={}):
     """
     request to reddit api
     """
-
     if not dic:
         for word in word_list:
-            dic.update({word: 0})
+            dic.update({word.lower(): 0})
     req = requests.get(
         "http://www.reddit.com/r/{}/hot.json?after={}".format(
             sys.argv[1], after),
@@ -27,6 +26,7 @@ def count_words(subreddit, word_list, after=None, dic={}):
         return
     json_text = json.loads(req.text)
     if json_text["data"]["after"] is None:
+        count(json_text["data"]["children"], 0, word_list, 0, 0, dic)
         sorted_dic = sorted(dic.items(),
                             key=operator.itemgetter(1), reverse=True)
         for value in sorted_dic:
@@ -40,15 +40,16 @@ def count(json, index, word_list, index_words, word_count, dic):
     """
     recursivly count the words from a title from a json
     """
-    c = 0
+    c = word_count
     if index_words >= len(word_list):
         return
     if index >= len(json):
         return count(json, 0, word_list, index_words + 1, 0, dic)
     c = [x for x in json[index]["data"]["title"].lower().split()].count(
         word_list[index_words].lower())
-    # print([x for x in json[index]["data"]["title"].lower().split()])
     if c > 0:
         word_count += c
-        dic.update({word_list[index_words]: dic[word_list[index_words]] + c})
+        dic.update(
+            {word_list[index_words].lower():
+                dic[word_list[index_words].lower()] + c})
     return count(json, index + 1, word_list, index_words, word_count, dic)
