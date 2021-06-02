406a239abc2ff028a0aa7b2466499ee6eefc4c9e
gefranco <933@holbertonschool.com>
2021-06-02 22:19:12 +0000

    [add] doc and fix betty

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * find_listint_loop -  finds the loop in a linked list
+ * @head: head linked list pointer
+ * Return: node where the loop starts, or NULL if there is no loop
+ */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *first;
@@ -14,6 +19,6 @@ listint_t *find_listint_loop(listint_t *head)
 		if (first == second)
 			return (first);
 	}
-	
-	return NULL;
+
+	return (NULL);
 }
