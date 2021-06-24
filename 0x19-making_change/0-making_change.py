c44e69aca8615b960ed3519128cfdb201bae88ec
gefranco <933@holbertonschool.com>
2021-06-24 14:56:31 +0000

    [add] initial solution

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+def makeChange(coins, total):
+
+    total_coins = 0
+    total_value = 0
+
+    while total_value != total:
+        if not coins:
+            return -1
+        max_coin = get_max_coin(coins)
+        while total_value < total:
+            total_value += max_coin
+            total_coins += 1
+            if (total_value > total):
+                total_coins -= 1
+                total_value -= max_coin
+                break
+
+    return total_coins
+
+
+def get_max_coin(coins):
+    max_coin = coins[0]
+    for coin in coins:
+        if coin > max_coin:
+            max_coin = coin
+    coins.remove(max_coin)
+    return max_coin
