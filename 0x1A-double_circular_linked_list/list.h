cb66147fd4a2afd07220975af44d0de1ac7769e7
gefranco <933@holbertonschool.com>
2021-07-01 16:21:33 +0000

    [add] headers

---
+++ 0x1A-double_circular_linked_list/list.h
@@ -0,0 +1,14 @@
+#ifndef _LIST_
+#define _LIST_
+
+typedef struct List
+{
+	char *str;
+	struct List *prev;
+	struct List *next;
+} List;
+
+List *add_node_end(List **list, char *str);
+List *add_node_begin(List **list, char *str);
+
+#endif
