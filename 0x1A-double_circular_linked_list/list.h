6f120b677ddac9d1e8e7ac0732525d8a0f8f24dd
gefranco <933@holbertonschool.com>
2021-07-01 16:27:50 +0000

    [add] doc

--- 0x1A-double_circular_linked_list/list.h
+++ 0x1A-double_circular_linked_list/list.h
@@ -1,6 +1,14 @@
 #ifndef _LIST_
 #define _LIST_
-
+/**
+ * struct List - doubly linked list
+ * @str: string - (malloc'ed string)
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
 typedef struct List
 {
 	char *str;
