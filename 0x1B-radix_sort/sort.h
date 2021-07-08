8d4936209d7c07de347d8ad7305df9d004b3381a
gefranco <933@holbertonschool.com>
2021-07-08 22:49:24 +0000

    [fix] betty

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -1,5 +1,19 @@
 #ifndef _SORT_
 #define _SORT_
+/**
+ * struct List - linked list
+ * @number: int - (malloc'ed number)
+ * @next: points to the next node
+ *
+ * Description: linked list node structure
+ */
+typedef struct List
+{
+	int number;
+	struct List *next;
+} List;
+List *addNumber(List *init, int number);
+int free_list(List *list);
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 #endif
