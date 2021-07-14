b42e000793bba493d2db251d2e05b28a83b2675b
gefranco <933@holbertonschool.com>
2021-07-14 23:46:30 +0000

    [add] function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    
+    perimeter = 0
+    for x in range(0, len(grid)):
+        for y in range(0, len(grid[x])):
+            print(grid[x][y], end='')
+            if grid[x][y] is 1:
+                if grid[x - 1][y] is 0:
+                    perimeter += 1
+                if grid[x + 1][y] is 0:
+                    perimeter += 1
+                if grid[x][y - 1] is 0:
+                    perimeter += 1
+                if grid[x][y + 1] is 0:
+                    perimeter += 1
+                
+        print("")            
+    return perimeter
