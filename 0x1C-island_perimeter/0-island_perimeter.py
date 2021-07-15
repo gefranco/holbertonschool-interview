62961ea5bfc4ac423fe1a1159d8caeb875a0b25f
gefranco <933@holbertonschool.com>
2021-07-15 14:23:14 +0000

    [testing]

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -12,13 +12,21 @@ def island_perimeter(grid):
     for x in range(0, len(grid)):
         for y in range(0, len(grid[x])):
             if grid[x][y] is 1:
-                if grid[x - 1][y] is 0:
+                if x - 1 < 0:
                     perimeter += 1
-                if grid[x + 1][y] is 0:
+                if x + 1 >= len(grid):
+                    perimeter += 1
+                if y - 1 < 0:
+                    perimeter += 1
+                if y + 1 >= len(grid[x]):
+                    perimeter += 1
+                if x - 1 >= 0 and grid[x - 1][y] is 0:
+                    perimeter += 1
+                if x + 1 < len(grid) and grid[x + 1][y] is 0:
                     perimeter += 1
                 if grid[x][y - 1] is 0:
                     perimeter += 1
-                if grid[x][y + 1] is 0:
+                if y + 1 < len(grid[x]) and grid[x][y + 1] is 0:
                     perimeter += 1
 
     return perimeter
