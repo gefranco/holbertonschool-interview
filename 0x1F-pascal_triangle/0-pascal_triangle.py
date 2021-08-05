a99e63eb9c1635652f1c54703e7fac59ad63c7cf
gefranco <933@holbertonschool.com>
2021-08-05 16:27:22 +0000

    [fix] pep8

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -2,18 +2,18 @@
 def pascal_triangle(n):
     vals = []
     i = 0
-    temp = 1;
+    temp = 1
     for i in range(0, n):
         inner_vals = []
         for j in range(0, i + 1):
             if j == 0 or j == i:
-                inner_vals.append(1);
+                inner_vals.append(1)
             else:
-                inner_vals.append(0);
+                inner_vals.append(0)
         vals.append(inner_vals)
     for i in range(0, n):
         for j in range(0, i + 1):
-            if i > 0 and (j != 0 and j!= i):
-                vals[i][j] = vals[i - 1][j - 1] + vals[i - 1][j];
+            if i > 0 and (j != 0 and j != i):
+                vals[i][j] = vals[i - 1][j - 1] + vals[i - 1][j]
         temp += 1
     return vals
