c05d369daca61a6cdf1d5eda6aba1b558689e211
gefranco <933@holbertonschool.com>
2021-08-12 22:47:28 +0000

    [fix] betty

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+
+/**
+ * regex_match - checks whether a given pattern matches a given string
+ * @str: the string to check
+ * @pattern: the pattern
+ * Return: 1 if the pattern matches the string, or 0 if it doesn’t
+ */
 int regex_match(char const *str, char const *pattern)
 {
 	int match = 0;
-	if (*str == '\0' &&  *pattern =='\0')
+
+	if (*str == '\0' &&  *pattern == '\0')
 		return (1);
 
-	
 	if ((*str == *pattern || *pattern == '.') && *(pattern + 1)  != '*')
 	{
-		return (regex_match(str +=1, pattern +=1));	
+		return (regex_match(str += 1, pattern += 1));
 	}
 	if (*(pattern + 1) == '*')
 	{
-		if(*str != '\0' && (*str == *pattern || *pattern == '.'))
-			match = regex_match(str +=1, pattern);
-		return (regex_match(str, pattern +=2) || match);
+		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+			match = regex_match(str += 1, pattern);
+		return (regex_match(str, pattern += 2) || match);
 	}
 	return (0);
 }
